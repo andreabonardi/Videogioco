fc65d80196d3e10ad11bae6477ec84dd1e6441b3
andreaabonardi <abona2000@hotmail.it>
2020-03-23 10:14:05 +0100

    creati i componenti base

--- Videogioco/src/app/app.module.ts
+++ Videogioco/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { EditGameComponent } from './components/edit-game/edit-game.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EditGameComponent,
+    GameDetailComponent
   ],
   imports: [
     BrowserModule,
